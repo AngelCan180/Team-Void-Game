25129390a5d2923bee41f958f8dcbc12f9d1fbea
AngelCan180 <126304755+AngelCan180@users.noreply.github.com>
2023-07-15 16:14:19 -0700

    7/15

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -7,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     public float climbSpeed = 3.0f;
     public float moveSpeed = 5.0f;
 
+    private bool isTrapActive = false;
     private Rigidbody2D rb;
 
     private void Start()
@@ -16,27 +18,30 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        if (canClimb)
+        if (!isTrapActive)
         {
-            climbTimer -= Time.deltaTime;
-
-            if (climbTimer <= 0)
+            if (canClimb)
             {
-                // Disable climbing ability
-                canClimb = false;
+                climbTimer -= Time.deltaTime;
+
+                if (climbTimer <= 0)
+                {
+                    // Disable climbing ability
+                    canClimb = false;
+                }
+                else
+                {
+                    // Climb up or down based on arrow key input
+                    float verticalInput = Input.GetAxis("Vertical");
+                    rb.velocity = new Vector2(rb.velocity.x, verticalInput * climbSpeed);
+                }
             }
             else
             {
-                // Climb up or down based on arrow key input
-                float verticalInput = Input.GetAxis("Vertical");
-                rb.velocity = new Vector2(rb.velocity.x, verticalInput * climbSpeed);
+                // Handle other player movements (left, right, etc.)
+                float horizontalInput = Input.GetAxis("Horizontal");
+                rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
             }
-        }
-        else
-        {
-            // Handle other player movements (left, right, etc.)
-            float horizontalInput = Input.GetAxis("Horizontal");
-            rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
         }
     }
 
@@ -48,5 +53,18 @@ public class PlayerMovement : MonoBehaviour
             canClimb = true;
             climbTimer = 0.5f;
         }
+        else if (collision.CompareTag("Trap"))
+        {
+            // Start trap effect
+            isTrapActive = true;
+            rb.velocity = Vector2.zero; // Stop player movement
+            StartCoroutine(DisableTrapAfterDelay());
+        }
+    }
+
+    private IEnumerator DisableTrapAfterDelay()
+    {
+        yield return new WaitForSeconds(3f);
+        isTrapActive = false;
     }
 }
