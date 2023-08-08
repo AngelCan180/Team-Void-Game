920870d1769938aed5408015a79e09bfb9daafb2
AngelCan180 <126304755+AngelCan180@users.noreply.github.com>
2023-08-07 23:27:39 -0700

    sounds

--- Assets/Key.cs
+++ Assets/Key.cs
@@ -5,16 +5,39 @@ using UnityEngine;
 public class Key : MonoBehaviour
 {
     public GameObject Door;
+    public Sprite blackColourSprite; // The new sprite
+    private AudioSource audioSource;
+    private bool keyPickedUp = false;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.CompareTag("player"))
+        if (col.CompareTag("player") && !keyPickedUp)
         {
             Debug.Log("Key Picked up");
-
             Door.GetComponent<BoxCollider2D>().enabled = false; 
+
+            // play sound
+            audioSource.Play();
+
+            keyPickedUp = true;
+
+            // Change the door sprite
+            SpriteRenderer doorSpriteRenderer = Door.GetComponent<SpriteRenderer>();
+            doorSpriteRenderer.sprite = blackColourSprite;
+
+            // Adjust the sprite to the size of the collider
+            BoxCollider2D doorCollider = Door.GetComponent<BoxCollider2D>();
+            Vector2 colliderSize = doorCollider.size;
+            doorSpriteRenderer.gameObject.transform.localScale = new Vector3(colliderSize.x / doorSpriteRenderer.sprite.bounds.size.x, colliderSize.y / doorSpriteRenderer.sprite.bounds.size.y, 1);
+
+            // Optional: Disable the key's collider or the key itself
+            // GetComponent<Collider2D>().enabled = false; // disable collider
+            // gameObject.SetActive(false); // disable key
         }
-
-
     }
 }
